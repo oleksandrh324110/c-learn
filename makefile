93911e8c8f8a65aef68471d22b6f5713612d8a12
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-11-24 10:04:06 +0200

    .

--- makefile
+++ makefile
@@ -1,5 +1,22 @@
-cc = gcc
-cflags = -std=c11 -Os -Wall
+CC = clang
+CFLAGS = -std=c11 -O0
+LDFLAGS =
 
-%: src/%.c
-	$(cc) $(cflags) -o bin/$@.exe $<
+SRCS = $(wildcard src/*.c) $(wildcard src/**/*.c) $(wildcard src/**/**/*.c) $(wildcard src/**/**/**/*.c)
+OBJS  = $(SRCS:.c=.o)
+
+all: compile link run
+
+compile: $(OBJS)
+
+%.o: %.c
+	$(CC) -c $< -o $@ $(CFLAGS)
+
+link:
+	$(CC) $(OBJS) -o bin/main $(LDFLAGS)
+
+run:
+	bin/main
+
+clean:
+	rm $(OBJS)
--- src/main.c
+++ src/main.c
@@ -1,45 +1,14 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-typedef int i32;
-typedef long long i64;
-typedef unsigned int u32;
-typedef unsigned long long u64;
-typedef float f32;
-typedef double f64;
-
-const u32 width = 120;
-const u32 height = 30;
-const f32 screen_aspect = (f32)width / height;
-const f32 character_aspect = (f32)9 / 20;
-const f32 aspect = screen_aspect * character_aspect;
+struct Human {
+  const char *name;
+  int age;
+  int height;
+  int weight;
+};
 
 int main() {
-  char buffer[width * height + 1];
-  buffer[width * height] = '\0';
-
-  for (u64 t = 0;; t++) {
-    for (u32 i = 0; i < height; i++) {
-      for (u32 j = 0; j < width; j++) {
-        f32 x = ((f32)j / width * 2 - 1) * aspect;
-        f32 y = (f32)i / height * 2 - 1;
-
-        x += sin(t * 0.03) * 0.5 * aspect;
-        y += cos(t * 0.03) * 0.5;
-
-        char pixel = ' ';
-        if (pow(x, 2) + pow(y, 2) < 0.2) {
-          pixel = '@';
-        }
-
-        buffer[i * width + j] = pixel;
-      }
-    }
-
-    printf(buffer);
-
-    usleep(1000000 / 120);
-  }
+  struct Human *man = malloc(sizeof(struct Human));
+  printf("%s\n", man->name);
 }
