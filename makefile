f5cd9c216496c5615d3341dccc35ff337a5ba024
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-02-04 21:16:50 +0200

    .

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 all:
-	gcc src/main.c -o bin/main.exe -std=c99
+	gcc src/main.c -o bin/main.exe -std=c99 -O0
 	bin/main.exe
--- src/main.c
+++ src/main.c
@@ -3,17 +3,4 @@
 #include <string.h>
 
 int main(int argc, const char** argv) {
-  if (argc < 2)
-    return 0;
-
-  char buff[3];
-
-  strncpy(buff, argv[1], 3);
-
-  if (!strncmp(buff, "cat", 3))
-    printf("meow");
-  else if (!strncmp(buff, "dog", 3))
-    printf("Woof");
-
-  return 0;
 }
