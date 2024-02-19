784e03c3e12e064c33f778eb55d306e4ca7abe4d
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-02-19 23:24:24 +0200

    .

--- src/main.c
+++ src/main.c
@@ -4,12 +4,17 @@
 
 bool isPrime(long long int n) {
   for (long long int i = 2; i < sqrt(n) + 1; i++)
-    if (n % 2 == 0)
+    if (n % i == 0)
       return false;
   return true;
 }
 
-int main() {
-  long long int n = 13;
-  printf("is prime %lld?: %s", n, isPrime(n) ? "yes" : "no");
+int main(void) {
+  long long int n;
+
+  while (scanf("%lld", &n) == 1) {
+    printf("%s\n", isPrime(n) ? "prime" : "not prime");
+  }
+
+  return 0;
 }
